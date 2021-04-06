0b4de772b675a17c04161ff9d9c5e0d9447e0e5d
Fajgri Espinoza <tfajgri@gmail.com>
2021-04-06 01:43:47 -0600

    Agregar archivos de proyecto.

---
+++ Aplicacion1/Controllers/HealthController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicacion1.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class HealthController : Controller
+	{
+		/*Path constant of the file to read.*/
+		private const string Path = "C:/temp/index.html";
+
+		[HttpGet("details")]
+		public IActionResult HeathDetails()
+		{
+			/* Method to read the html and get the value of the src = to filter */
+		   var str = System.IO.File.ReadAllText(Path);
+		
+			int start = str.IndexOf("src=\"main.") + 5;
+			
+			string result = str.Substring(start, str.IndexOf("\"", start) - start);
+
+			return Ok(new { JsVersion = result });
+		}
+
+	}
+}
